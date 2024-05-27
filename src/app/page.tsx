ca60f8161f35f0f020c2d31acbddf89f32af3d38
Mohammed <73973634+mo-balsara@users.noreply.github.com>
2024-05-27 17:43:28 -0400

    semicolon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <main>
         <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
         {/* Other content */}
       </main>
     </div>
   );
-}
+};
