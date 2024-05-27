3ac0f6555330089f572a6d9b97b4f32c2aed09fd
Mohammed <73973634+mo-balsara@users.noreply.github.com>
2024-05-27 18:28:45 -0400

    mm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import Navbar from '../components/Navbar'; 
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <main>
         <link rel="icon" href="public/favicon.jpg" />
         <h1 className="text-4xl font-bold">TaskTrack - Your productivity based calender app</h1>
